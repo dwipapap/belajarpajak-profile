98a366e4b8656c88ba995aad339fc2198b74d24c
dwipapap <dwipahyaki@gmail.com>
2026-08-02 08:22:24 +0700

    perf: serve 240px webp logo, keep png fallback

--- app/components/BrandLogo.vue
+++ app/components/BrandLogo.vue
@@ -6,12 +6,18 @@ defineProps<{
 </script>
 
 <template>
-  <img
-    src="/brand/logo-kbs.png"
-    :alt="`Logo ${COMPANY.legalName}`"
-    :height="height ?? 40"
-    :width="Math.round(((height ?? 40) * 687) / 292)"
-    class="h-auto w-auto"
-    :style="{ height: `${height ?? 40}px` }"
-  >
+  <picture>
+    <source
+      type="image/webp"
+      srcset="/brand/logo-kbs-sm.webp"
+    >
+    <img
+      src="/brand/logo-kbs.png"
+      :alt="`Logo ${COMPANY.legalName}`"
+      :height="height ?? 40"
+      :width="Math.round(((height ?? 40) * 687) / 292)"
+      class="h-auto w-auto"
+      :style="{ height: `${height ?? 40}px` }"
+    >
+  </picture>
 </template>
